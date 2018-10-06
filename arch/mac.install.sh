f3e4990ed288a665c47cbe275c593a89df167438
Huajian Mao <huajianmao@gmail.com>
2018-10-06 14:21:52 +0800

    Revise zsh setup script

--- arch/mac.install.sh
+++ arch/mac.install.sh
@@ -6,7 +6,7 @@ HOSTNAME=mini
 git
 
 # 2. change the hostname to whatever you like
-sudo scutil –-set HostName $HOSTNAME
+sudo scutil --set HostName $HOSTNAME
 sudo scutil --set LocalHostName $HOSTNAME
 # Change the Mac's computer name
 # in System Preference -> Sharing
@@ -25,10 +25,16 @@ ssh-keygen -t rsa
 # 5. install Apps
 # You may install shadowsocksx-ng first for the network connection
 
+# https://github.com/senta/.dotfiles/blob/master/bootstrap.sh
+
 # You may need to set PROXY for some package downloading
 # export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
 
 BREW_APPS=(
+  dos2unix
+  mas
+  tmux
+  tree
   unrar
   vim
   wget
@@ -61,9 +67,7 @@ BREW_CASK_APPS=(
 brew install ${BREW_APPS[@]}
 
 brew cask install --appdir="/Applications" ${BREW_CASK_APPS[@]}
-brew cask alfred link
 
-brew cleanup --force
 rm -rf /Library/Caches/Homebrew/*
 
 
@@ -71,16 +75,33 @@ rm -rf /Library/Caches/Homebrew/*
 # 6. set default preferences
 ############################
 # You may refer to https://gist.github.com/tpinto/2420293
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
+
 defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
 # proxy
 # power save time
 # keyboard command / option switch
 
+# Terminal
+# terminal close on exit
+# color theme to solarized dark
+
+# set default shell to zsh
+
+
+####################################
+# 7. install Apps from Mac App Store
+####################################
+# You will need to login first:
+# mas signin apple.id@gmail.com "password"
+mas install 497799835 # Xcode
 
 #############################
-# 7. Add Apps to Startup list
+# 8. Add Apps to Startup list
 #############################
 # shadowsocksx-ng
+# Alfred
 
 
 
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,9 +6,6 @@ CONFIG=`pwd`/dotzshrc
 OMZRC=~/.oh-my-zsh
 OHMYZSH=`pwd`/oh-my-zsh
 
-FONTFILE=`pwd`/PowerlineSymbols.otf
-FONTDIR=/usr/share/fonts/extra
-
 if [ -f $RCFILE ];
 then
   mv $RCFILE $RCFILE.`date +%Y-%m-%d_%H:%M:%S`
@@ -21,10 +18,26 @@ then
 fi
 # ln -s $OHMYZSH $OMZRC
 
-echo "To install patch font into $FONTDIR, root privilliage needed!"
-sudo mkdir -p $FONTDIR
-sudo cp $FONTFILE $FONTDIR
-sudo fc-cache -fv
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+	    FONTFILE=`pwd`/PowerlineSymbols.otf
+	    FONTDIR=/usr/share/fonts/extra
+	    echo "To install patch font into $FONTDIR, root privilliage needed!"
+	    sudo mkdir -p $FONTDIR
+	    sudo cp $FONTFILE $FONTDIR
+	    sudo fc-cache -fv
+	    ;;
+    Darwin*)
+	    echo "In Mac OS X, no need to install fonts"
+	    ;;
+    CYGWIN*)
+	    ;;
+    MINGW*)
+	    ;;
+    *)
+	    ;;
+esac
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git $OMZRC
 cp hjmao.zsh-theme $OMZRC/themes/

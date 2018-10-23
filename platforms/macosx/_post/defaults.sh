e517d583299fab5d4e970b7e20d6631b08d7fdb1
Huajian Mao <huajianmao@gmail.com>
2018-10-23 17:50:00 +0800

    Add python libraries

--- platforms/macosx/_post/defaults.sh
+++ platforms/macosx/_post/defaults.sh
@@ -9,6 +9,10 @@ exit
 # You may refer to https://gist.github.com/tpinto/2420293
 # https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 # https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
+
+# Enable full keyboard access for all controls
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 chsh -s /bin/zsh
---
+++ platforms/macosx/_post/python.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "In partial/python"
+exit
+
+pip3 install pandas numpy matplotlib seaborn

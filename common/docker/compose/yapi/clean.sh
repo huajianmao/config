0b55b1b902982d81ec06e34cb0be065c73680c73
Huajian Mao <huajianmao@gmail.com>
2020-03-20 11:09:02 +0800

    Add yapi docker configuration

---
+++ common/docker/compose/yapi/clean.sh
@@ -0,0 +1,5 @@
+docker stop yapi
+docker rm yapi
+docker stop yapidb
+docker rm yapidb
+sudo rm -rf ../../volumes/yapi
---
+++ common/docker/compose/yapi/init.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+mkdir -p ../../volumes/yapi/db
+mkdir -p ../../volumes/yapi/data
+

aed5a35a685817b8180a59985ebd03002d400cef
Huajian Mao <huajianmao@gmail.com>
2020-03-20 21:27:31 +0800

    Add yapi dockerwq

--- common/docker/compose/yapi/init.sh
+++ common/docker/compose/yapi/init.sh
@@ -2,3 +2,12 @@
 mkdir -p ../../volumes/yapi/db
 mkdir -p ../../volumes/yapi/data
 
+docker run -d --name yapidb --volume `pwd`/../../volumes/yapi/db:/data/db mongo
+
+docker run -it --rm \
+  --link yapidb:mongo \
+  --entrypoint npm \
+  --workdir /api/vendors \
+  registry.cn-hangzhou.aliyuncs.com/anoy/yapi \
+  run install-server
+

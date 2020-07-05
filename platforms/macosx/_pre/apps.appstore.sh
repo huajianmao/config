22326bbd477a8a10ccf521d840be4192b8d265b6
Huajian Mao <huajianmao@gmail.com>
2020-07-05 10:03:59 +0800

    Add pixea image viewer for mac

--- platforms/macosx/_pre/apps.appstore.sh
+++ platforms/macosx/_pre/apps.appstore.sh
@@ -10,5 +10,6 @@ echo "In _pre/apps.appstore"
 # You will need to login first:
 # mas signin apple.id@gmail.com "password"
 mas install 497799835 # Xcode
-mas install 1176895641 # Spark
-mas install 1518036000 # Sequel Ace
+mas install 1176895641 # Spark Email
+mas install 1518036000 # Sequel Ace Mysql Client
+mas install 1507782672 # Pixea image viewer
